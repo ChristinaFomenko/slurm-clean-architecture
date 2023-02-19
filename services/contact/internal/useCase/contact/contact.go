0009649ba209312327f23b120a5a29f32f42327a
Christina Fomenko <kris.inferno@gmail.com>
2023-02-19 12:32:47 +0300

    usecase impl

--- services/contact/internal/useCase/contact/contact.go
+++ services/contact/internal/useCase/contact/contact.go
@@ -2,37 +2,45 @@ package contact
 
 import (
 	"github.com/google/uuid"
+	"time"
 
 	"github.com/ChristinaFomenko/slurm-clean-architecture/pkg/type/queryParameter"
 	"github.com/ChristinaFomenko/slurm-clean-architecture/services/contact/internal/domain/contact"
 )
 
 func (uc *UseCase) Create(contacts ...*contact.Contact) ([]*contact.Contact, error) {
-	// TODO implement me
-	panic("implement me")
+	return uc.adapterStorage.CreateContact()
 }
 
 func (uc *UseCase) Update(contactUpdate contact.Contact) (*contact.Contact, error) {
-	// TODO implement me
-	panic("implement me")
+	return uc.adapterStorage.UpdateContact(contactUpdate.ID(), func(oldContact *contact.Contact) (*contact.Contact, error) {
+		return contact.NewWithID(
+			oldContact.ID(),
+			oldContact.CreatedAt(),
+			time.Now().UTC(),
+			contactUpdate.PhoneNumber(),
+			contactUpdate.Email(),
+			contactUpdate.Name(),
+			contactUpdate.Surname(),
+			contactUpdate.Patronymic(),
+			contactUpdate.Age(),
+			contactUpdate.Gender(),
+		)
+	})
 }
 
 func (uc *UseCase) Delete(ID uuid.UUID) error {
-	// TODO implement me
-	panic("implement me")
+	return uc.adapterStorage.DeleteContact(ID)
 }
 
 func (uc *UseCase) List(parameter queryParameter.QueryParameter) ([]*contact.Contact, error) {
-	// TODO implement me
-	panic("implement me")
+	return uc.adapterStorage.ListContact(parameter)
 }
 
 func (uc *UseCase) ReadByID(ID uuid.UUID) (response *contact.Contact, err error) {
-	// TODO implement me
-	panic("implement me")
+	return uc.adapterStorage.ReadContactByID(ID)
 }
 
 func (uc *UseCase) Count() (uint64, error) {
-	// TODO implement me
-	panic("implement me")
+	return uc.adapterStorage.CountContact()
 }
